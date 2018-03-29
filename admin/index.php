00391a4e0be748bfcd0277dabe6c74348b3d9993
yuv <voronayu1331@gmail.com>
2018-03-29 19:39:18 +0300

    5

--- admin/index.php
+++ admin/index.php
@@ -34,17 +34,16 @@ if(isset($_POST)){
 //    }
     //var_dump($_FILES['img']);
 
-//    $path = 'uploads_img/';
-//
-//    if ($_SERVER['REQUEST_METHOD'] == 'POST')
-//    {
-//        if (!@copy($_FILES['img']['tmp_name'], $path . $_FILES['img']['name']))
-//            echo 'Что-то пошло не так';
-//        else
-//            echo 'Загрузка удачна';
-//    }
+    $path = realpath('uploads_img')."/";
 
-    echo realpath('uploads_img/');
+    if ($_SERVER['REQUEST_METHOD'] == 'POST')
+    {
+        if (!@copy($_FILES['img']['tmp_name'], $path . $_FILES['img']['name']))
+            echo 'Что-то пошло не так';
+        else
+            echo 'Загрузка удачна';
+    }
+
 }
 
 ?>
