2d93072fbbb99360d3e07ffa0014ef637a2ab343
yuv <voronayu1331@gmail.com>
2018-04-25 20:28:26 +0300

    17.02

--- function.php
+++ function.php
@@ -53,7 +53,7 @@ function calc_oc4($a, $b){
     if($b > $a){
         $result = round(100 * ((abs($b) - abs($a))/$a), 2);
     }else{
-        $result = round(100 * ((abs($a) - abs($b))/$b), 2);
+        $result = round(100 * ((abs($a) - abs($b))/$a), 2);
     }
 
 
